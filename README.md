4266be4a1747cb8666b6b2a55c339cc0bc809508
Charles Eckman <charles@cceckman.com>
2024-06-19 14:00:43 -0400

    Accretion

--- README.md
+++ README.md
@@ -1,12 +1,4 @@
-# fiona
+# Accretion
 
-Creative Coding, 2024-05-29
+Creative Coding, 2024-06-19
 
-> The air became Fiona, and deserving—no, demanding—of love.
-
-Neal Stephenson, _The Diamond Age_
-
-Query parameters:
-
-- `name` to set the string used
-- `load` to set the particle density -- use _small fractions_, e.g. 0.001
--- app.js
+++ app.js
@@ -2,17 +2,17 @@
 
 // Get the name we're forming.
 const searchParams = new URLSearchParams(window.location.search);
-let render_name = "Fiona";
+let render_name = "ACCRETION";
 if (searchParams.has("name")) {
     render_name = searchParams.get("name")
 }
-let load = 0.0017;
+let load = 0.0007;
 if (searchParams.has("load")) {
     load = parseFloat(searchParams.get("load"))
 }
 
-const canvas = document.getElementById("draw-here");
-const bg_canvas = document.getElementById("dont-draw-here");
+const canvas = document.getElementById("accretion-canvas");
+const bg_canvas = document.getElementById("accretion-bg-canvas");
 
 function clamp(x) {
     if (x < 0) {
@@ -28,7 +28,13 @@ let x = []
 let y = []
 let vx = []
 let vy = []
-let scales = []
+let slowdown = 0.95;
+let min = 0.00001;
+let initScale = 0.2;
+let sizeScale = 0.002;
+let cleanupTimeout = 5000;
+
+let stopped = [];
 
 // TODO: Consider using a Gaussian distribution
 // instead of whatever Math.random() is
@@ -44,20 +50,20 @@ function add_particle() {
     } else {
         xsign = 1;
     }
-    if (Math.random() > 0.5) {
-        ysign = -1;
-    } else {
-        ysign = 1;
-    }
+    // Only falling...
+    //  if (Math.random() > 0.5) {
+    //      ysign = -1;
+    //  } else {
+    //      ysign = 1;
+    //  }
+    ysign = 1;
     // at most this fraction of the screen 
     // per millisecond
-    vx.push(Math.random() * 0.5 * xsign)
-    vy.push(Math.random() * 0.5 * ysign)
-
-    scales.push(1)
+    vx.push(Math.random() * initScale * xsign)
+    vy.push(Math.random() * initScale * ysign)
 }
 
-function update_scales() {
+function update_speed() {
     let name = bg_canvas.getContext("2d")
 
     for (let i = 0; i < x.length; i++) {
@@ -67,16 +73,37 @@ function update_scales() {
         let px = name.getImageData(xx, yy, 1, 1).data;
         // console.log("pixel data: ", px);
         if (px[3] !== 0) {
-            scales[i] = 0.1
-        } else {
-            scales[i] = 1
+            // Slow down this pixel.
+            vx[i] *= slowdown;
+            vy[i] *= slowdown;
         }
     }
 }
 
+function flush_stopped() {
+    let x_ = [];
+    let y_ = [];
+    let vx_ = [];
+    let vy_ = [];
+    for (let i = 0; i < x.length; i++) {
+        if (Math.abs(vx[i]) > min && Math.abs(vy[i]) > min) {
+            x_.push(x[i])
+            y_.push(y[i])
+            vx_.push(vx[i])
+            vy_.push(vy[i])
+        } else {
+            stopped.push({ x: x[i], y: y[i] });
+        }
+    }
+    x = x_;
+    y = y_;
+    vx = vx_;
+    vy = vy_;
+}
 
-function update_one(elapsed, m, vm, i) {
-    let d = vm[i] * elapsed * scales[i];
+
+function bounce(elapsed, m, vm, i) {
+    let d = vm[i] * elapsed;
     let mm = m[i] + d;
     if (mm < 0 || mm > 1.0) {
         // Don't move on this tick,
@@ -87,30 +114,49 @@ function update_one(elapsed, m, vm, i) {
     }
 }
 
-function update_all(elapsed) {
-    update_scales()
-    for (let i = 0; i < x.length; i++) {
-        update_one(elapsed, x, vx, i);
-    }
-    for (let i = 0; i < y.length; i++) {
-        update_one(elapsed, y, vy, i);
+function rotate(elapsed, m, vm, i) {
+    let d = vm[i] * elapsed;
+    let mm = m[i] + d;
+    if (mm < 0) {
+        m[i] = mm + 1;
+    } else if (mm > 1) {
+        m[i] = mm - 1;
+    } else {
+        m[i] = mm
     }
 }
 
+function update_all(elapsed) {
+    update_speed()
+    for (let i = 0; i < x.length; i++) {
+        bounce(elapsed, x, vx, i);
+    }
+    for (let i = 0; i < y.length; i++) {
+        rotate(elapsed, y, vy, i);
+    }
+}
+
+let draw_count = 0;
 function draw(ctx) {
     ctx.reset()
-    ctx.fillStyle = "blue";
+    ctx.fillStyle = "brown";
+    const min = Math.min(canvas.height, canvas.width)
+    let size = Math.max(Math.round(min * sizeScale), 2)
     for (let i = 0; i < x.length; i++) {
-        const min = Math.min(canvas.height, canvas.width)
-        let size = Math.min(Math.round(min * 0.1), 2)
-
-        const xx = x[i] * canvas.width
-        const yy = y[i] * canvas.height
-        ctx.beginPath()
-        ctx.ellipse(xx, yy, size, size, 0, 0, 2 * Math.PI)
-        ctx.fill()
-        ctx.closePath()
+        draw_point(ctx, size, x[i], y[i]);
     }
+    for (let { x, y } of stopped) {
+        draw_point(ctx, size, x, y);
+    }
+}
+
+function draw_point(ctx, size, x, y) {
+    const xx = x * canvas.width
+    const yy = y * canvas.height
+    ctx.beginPath()
+    ctx.ellipse(xx, yy, size, size, 0, 0, 2 * Math.PI)
+    ctx.fill()
+    ctx.closePath()
 }
 
 function draw_name() {
@@ -127,9 +173,8 @@ function draw_name() {
     ctx.fillText(render_name, canvas.width / 2, textY, canvas.width)
 }
 
-let first_draw = false;
 function handle_resize() {
-    if (!first_draw || canvas.height != canvas.clientHeight
+    if (draw_count > 0 || canvas.height != canvas.clientHeight
         || canvas.width != canvas.clientWidth) {
         canvas.height = canvas.clientHeight;
         canvas.width = canvas.clientWidth;
@@ -140,27 +185,36 @@ function handle_resize() {
         // Figure out how many particles to draw,
         // by a load factor.
         let count = Math.round(canvas.height * canvas.width * load)
+        if (stopped.length > count) {
+            stopped.length = count;
+        }
+        count -= stopped.length;
         while (x.length > count) {
             x.length = count
             y.length = count
             vx.length = count
             vy.length = count
-            scales.length = count
         }
+
         while (x.length < count) {
             add_particle()
         }
     }
 }
 
-
 let zero = document.timeline.currentTime;
+let lastCleanup = document.timeline.currentTime;
 function redraw(last_frame) {
     handle_resize()
 
     let elapsed = last_frame - zero;
     zero = last_frame;
     update_all(elapsed / 1000);
+    let cleanup = last_frame - lastCleanup;
+    if (cleanup > cleanupTimeout) {
+        lastCleanup = last_frame;
+        flush_stopped();
+    }
 
     let ctx = canvas.getContext("2d")
     draw(ctx);
